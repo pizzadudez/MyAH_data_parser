fd800dec17a10b48eb0be9f4173c2ca35c7131cd
pizzadudez <40966394+pizzadudez@users.noreply.github.com>
2019-06-19 15:19:35 +0300

    refactoring progress

--- auction_data.py
+++ auction_data.py
@@ -27,11 +27,14 @@ class Realm:
         self.position = row[9]
         self.account = row[10]
 
+    def __str__(self):
+        return self.name
+
     def update_db(self):
         """Updates Realm instance's row"""
         conn = sqlite3.connect(REALM_DB)
         c = conn.cursor()
-        c.execute("""UPDATE realms SET last_update = ?, last_checked = ?
+        c.execute("""UPDATE realms SET last_update = ?, last_check = ?
                 WHERE name = ? AND (last_Update != ? OR last_update IS NULL)""",
                 (self.last_update, self.last_check, self.name, self.last_update))
         conn.commit()
@@ -45,6 +48,9 @@ class Item:
         self.category_id = row[3]
         self.position = row[4]
         self.stack_sizes = tuple(row[5])
+
+    def __str__(self):
+        return self.name
 
 
 class DataParser:
@@ -80,7 +86,7 @@ class DataParser:
                 c.execute("SELECT stack_size FROM stack_sizes WHERE category_id = ?",
                         (row[3], ))
                 stack_sizes = [x[0] for x in c.fetchall()]
-                items[row[1]] = row + (stack_sizes, )
+                items[row[1]] = Item(row + (stack_sizes, ))
             return items
 
         self.wowapi = wowapi(CLIENT_ID, CLIENT_SECRET)
@@ -93,23 +99,145 @@ class DataParser:
         Set 'force_update=True' to bypass update constraints. This will
         update all realms, even those that are already up to date!
         """
-        pass
+        processes = []
+        queue = Queue() # process return values go here
+
+        # Start multiprocessing
+        print(">> Starting concurent update...")
+        for realm in self.realms.values():
+            if self.check_for_update(realm):
+                process = Process(target=self.update_realm, args=(realm, queue))
+                processes.append(process)
+                process.start()
+        # Join processes
+        for process in processes:
+            process.join()
+
+        # Deserialize data from finished worker processes
+        while not queue.empty():
+            realm = self.realms[queue.get()]
+            with open(f"{TEMP_FOLDER}/{realm.slug}.pickle", 'rb') as file:
+                self.parsed_data[realm.name] = pickle.load(file)
+            realm.update_db() # update Realm's db record
+
+        # TODO: add some sort of realms to update in model list, we're not rewriting the files anymore
+        self.write_output()
+        print("\nFinished concurent update!")
 
     def update_loop(self):
-        """Infinite update loop. Not concurent."""
-        pass
+        """Infinite update loop. No concurrent updates."""
+        # List of Realm objects, sorted by their expected next update timestamp
+        update_queue = [x for x in self.realms.values()]
+        update_queue.sort(key=lambda x: x.last_update + x.update_interval)
+
+        print("\n>> Starting update loop...")
+        while True:
+            sleep_ammount = None
+            next_update = None
+            for realm in update_queue:
+                time_delta = realm.last_update + realm.update_interval - round(time.time())
+                # We only update 3 seconds after the anticipated next_update
+                # to avoid late updates from blizz and time.time() rounding 
+                if time_delta >= -3:
+                    sleep_ammount = time_delta + 4
+                    next_update = realm
+                    break
+                elif time_delta < -3 and self.check_for_update(realm):
+                    self.parsed_data[realm.name] = self.update_realm(realm)
+                    self.write_output() # TODO pass realm as argument or smth
+                    realm.update_db() # everything went well, update Realm's db record
+                    break # Should this break even be here???
+                else:
+                    pass
+                    # TODO: do something with realms that dont have new data even if they should
+
+            # Resort the queue after breaking the loop
+            update_queue.sort(key=lambda x: x.last_update + x.update_interval)
+
+            if sleep_ammount:
+                sleep_time = time.strftime("%M:%S", time.gmtime(sleep_ammount))
+                print(f"\n> Next update: {next_update.name} in {sleep_time}.")
+                time.sleep(sleep_ammount)
+
 
     def update_realm(self, realm, queue=None):
         """Fetches the latest API json dump and parses it.\n
         Multiprocessing Queue is None by default.
         """
-        pass
+        print(f"{realm.name} updating...")
+        conn = sqlite3.connect(f"{TEMP_FOLDER}/{realm.slug}.sqlite3")
+        c = conn.cursor()
+        json_data = requests.get(realm.json_link).json()
+        # TODO: catch errors here
+
+        # Create or truncate table then dump json content in it
+        c.execute("""CREATE TABLE IF NOT EXISTS auctions (
+                auc_id INTEGER,
+                item_id INTEGER,
+                owner TEXT,
+                buyout INTEGER,
+                stack_size INTEGER,
+                time_left TEXT)""")
+        c.execute("DELETE FROM auctions")
+        for row in json_data['auctions']:
+            owner_and_realm = "-".join([row['owner'], row['ownerRealm'].replace(' ', '')])
+            c.execute("""INSERT INTO auctions (auc_id, item_id, owner, buyout, stack_size, time_left)
+                    VALUES (?, ?, ?, ?, ?, ?)""",
+                    (row['auc'], row['item'], owner_and_realm, row['buyout'], row['quantity'], row['timeLeft']))
+        conn.commit()
+
+        # Cluster relevant auctions based on price, stack_size, owner and time_left
+        auction_chunks = []
+        for item in self.items.values():
+            c.execute(f"""SELECT DISTINCT buyout, stack_size, owner, time_left FROM auctions
+                    WHERE item_id=? AND stack_size IN {item.stack_sizes}
+                    ORDER BY (buyout/stack_size) ASC""", (item.item_id, ))
+            for combo in c.fetchall():
+                buyout = combo[0]
+                stack_size = combo[1]
+                owner = combo[2]
+                time_left = combo[3]
+                price = round(buyout / stack_size) / 10000
+
+                c.execute("SELECT auc_id FROM auctions WHERE item_id=? AND buyout=? AND stack_size=? AND owner=? AND time_left=?",
+                        (item.item_id, buyout, stack_size, owner, time_left))
+                auc_ids = [x[0] for x in c.fetchall()]
+                quantity = len(auc_ids)
+
+                # Add cluster to list
+                cluster = {'item_id': item.item_id,
+                           'quantity': quantity,
+                           'price': price, 
+                           'stack_size': stack_size,
+                           'owner': owner, 
+                           'time_left': time_left,
+                           'auc_ids': auc_ids,}
+                auction_chunks.append(cluster)
+
+        print(f"> Finished updating: {realm.name}")
+        if queue:
+            # If subprocess serialize data and add realm name to queue
+            with open(f"{TEMP_FOLDER}/{realm.slug}.pickle", 'wb') as file:
+                pickle.dump(auction_chunks, file)
+            queue.put(realm.name)
+        else:
+            # Else return list of auction_chunks
+            return auction_chunks
 
     def check_for_update(self, realm):
-        """Checks for new json dump.\n
+        """Checks for new json dump and returns True if an update is needed.\n
         Updates Realm instance's 'last_update' field but not in the source file.
         """
-        pass
+        headers = self.wowapi.get_auctions('eu', realm.slug, locale='en_US')['files'][0]
+        last_update = headers['lastModified'] // 1000
+        realm.last_check = round(time.time())
+
+        if realm.last_update and realm.last_update == last_update:   
+            return False # update not available
+        
+        # Update realm's attribute in the db only after updating is done
+        realm.last_update = last_update
+        return True # update available
 
     def write_output(self):
         """Updates model with up to date parsed data.\n
@@ -121,4 +249,4 @@ class DataParser:
 
 if __name__ == '__main__':
     dp = DataParser()
-    print(dp.parsed_data)
+    dp.update_loop()
