a0e7304c22c961e9a4fdf65f4d3139c08df6599d
pizzadudez <40966394+pizzadudez@users.noreply.github.com>
2019-06-19 13:51:41 +0300

    helper classes and __init__

--- auction_data.py
+++ auction_data.py
@@ -14,17 +14,37 @@ from settings import *
 
 
 class Realm:
-    def __init__(self):
-        pass
+    def __init__(self, row):
+        self.name = row[0]
+        self.slug = row[1]
+        self.code = row[2]
+        self.update_interval = row[3]
+        self.last_update = row[4]
+        self.last_check = row[5]
+        self.json_link = row[6]
+        self.seller = row[7]
+        self.region = row[8]
+        self.position = row[9]
+        self.account = row[10]
 
     def update_db(self):
         """Updates Realm instance's row"""
-        pass
+        conn = sqlite3.connect(REALM_DB)
+        c = conn.cursor()
+        c.execute("""UPDATE realms SET last_update = ?, last_checked = ?
+                WHERE name = ? AND (last_Update != ? OR last_update IS NULL)""",
+                (self.last_update, self.last_check, self.name, self.last_update))
+        conn.commit()
 
 
 class Item:
-    def __init__(self):
-        pass
+    def __init__(self, row):
+        self.item_id = row[0]
+        self.name = row[1]
+        self.short_name = row[2]
+        self.category_id = row[3]
+        self.position = row[4]
+        self.stack_sizes = tuple(row[5])
 
 
 class DataParser:
@@ -32,7 +52,41 @@ class DataParser:
     for 'MyAH' (django webapp) and 'Multiboxer' (auction house addon).
     """
     def __init__(self):
-        pass
+        def load_last_session():
+            try:
+                with open(f"{TEMP_FOLDER}/_serialized_data.pickle", 'rb') as old:
+                    return pickle.load(old)
+            except:
+                return {}
+
+        def realm_objects_dict():
+            conn = sqlite3.connect(REALM_DB)
+            c = conn.cursor()
+            c.execute("SELECT * FROM realms")
+
+            realms = {}
+            for row in c.fetchall():
+                realms[row[0]] = Realm(row)
+            conn.close()
+            return realms
+
+        def item_objects_dict():
+            conn = sqlite3.connect(ITEM_DB)
+            c = conn.cursor()
+            c.execute("SELECT * FROM items")
+
+            items = {}
+            for row in c.fetchall():
+                c.execute("SELECT stack_size FROM stack_sizes WHERE category_id = ?",
+                        (row[3], ))
+                stack_sizes = [x[0] for x in c.fetchall()]
+                items[row[1]] = row + (stack_sizes, )
+            return items
+
+        self.wowapi = wowapi(CLIENT_ID, CLIENT_SECRET)
+        self.parsed_data = load_last_session()
+        self.realms = realm_objects_dict()
+        self.items = item_objects_dict()
 
     def update_all(self, force_update=False):
         """Concurently update all realms with old data.\n
@@ -67,4 +121,4 @@ class DataParser:
 
 if __name__ == '__main__':
     dp = DataParser()
-    dp.update_all()
+    print(dp.parsed_data)
